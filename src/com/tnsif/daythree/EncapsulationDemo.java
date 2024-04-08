027dfdcaaa468bbf7d9dceecf810d3b40c60d97e
Abdul Rahman <abdulrahmanasus@outlook.com>
2024-04-08 12:40:41 +0530

    day three- encapsulation

---
+++ src/com/tnsif/daythree/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.daythree;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          OOPSConceptDemo ocd = new OOPSConceptDemo();
+          ocd.setSerialNum(100);
+          ocd.setAge(30);
+          ocd.setName("Abu");
+          System.out.println(ocd);
+	}
+
+}
